dc5654f20eac36d67c3f8c0a0e8d1b7797ab1e09
Andreea Angelescu <anda.angelescu@gmail.com>
2024-05-21 15:32:55 +0300

    Implemented Admin's Producers page

---
+++ Business/ProducersService.cs
@@ -0,0 +1,43 @@
+﻿using Supermarket.DataAccess;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace Supermarket.Business
+{
+    public class ProducersService
+    {
+
+        SupermarketEntities _context;
+
+        public ProducersService()
+        {
+            _context = new SupermarketEntities();
+        }
+
+        public void Add(Producer producer)
+        {
+            _context.spCreateProducer(producer.ProducerName, producer.ProducerCountry);
+        }
+
+        public void Update(Producer producer)
+        {
+            var producerFound = _context.Producers.First(p => p.ProducerId == producer.ProducerId);
+
+            producerFound.ProducerName = producer.ProducerName;
+            producerFound.ProducerCountry = producer.ProducerCountry;
+
+            _context.SaveChanges();
+        }
+
+        public ObservableCollection<Producer> GetAll()
+        {
+            return new ObservableCollection<Producer>(_context.Producers.ToList());
+        }
+
+        public void Delete(int id)
+        {
+            _context.spDeleteProducer(id);
+        }
+
+    }
+}
--- ViewModels/ProducerViewModel.cs
+++ ViewModels/ProducerViewModel.cs
@@ -1,6 +1,139 @@
-﻿namespace Supermarket.ViewModels
+﻿using GalaSoft.MvvmLight.Command;
+using Supermarket.Business;
+using Supermarket.DataAccess;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace Supermarket.ViewModels
 {
-    internal class ProducerViewModel : BaseViewModel
+    public class ProducerViewModel : BaseViewModel
     {
+        private readonly ProducersService producersService;
+        public ObservableCollection<Producer> producers;
+        private Producer _selectedProducer;
+        private string name;
+        private string country;
+
+        public string Name
+        {
+            get => name;
+            set
+            {
+                name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+
+        public string Country
+        {
+            get => country;
+            set
+            {
+                country = value;
+                OnPropertyChanged(nameof(Country));
+            }
+        }
+
+        public Producer SelectedProducer
+        {
+            get => _selectedProducer;
+            set
+            {
+                if (_selectedProducer != value)
+                {
+                    _selectedProducer = value;
+                    OnPropertyChanged(nameof(SelectedProducer));
+                    Name = SelectedProducer.ProducerName;
+                    Country = SelectedProducer.ProducerCountry;
+                }
+            }
+        }
+
+        public ObservableCollection<Producer> Producers
+        {
+            get => producers;
+
+            set
+            {
+                producers = value;
+                OnPropertyChanged(nameof(Producers));
+            }
+        }
+
+        public ICommand AddProducer;
+        public ICommand EditProducer;
+        public ICommand DeleteProducer;
+        public ICommand SaveProducer;
+        public ICommand RefreshFields;
+
+        public ProducerViewModel()
+        {
+            producersService = new ProducersService();
+            Producers = producersService.GetAll();
+        }
+
+        public ICommand AddProducerCommand
+        {
+            get
+            {
+                return AddProducer ?? (AddProducer = new RelayCommand(() =>
+                {
+                    Producer newProducer = new Producer() { ProducerName = Name, ProducerCountry = Country };
+
+                    producersService.Add(newProducer);
+                    SelectedProducer = newProducer;
+                    Producers = producersService.GetAll();
+                    Name = "";
+                    Country = "";
+                }));
+            }
+
+        }
+
+        public ICommand EditProducerCommand
+        {
+            get
+            {
+                return EditProducer ?? (EditProducer = new RelayCommand(() =>
+                {
+                    producersService.Update(SelectedProducer);
+                    Producers = producersService.GetAll();
+                    Name = "";
+                    Country = "";
+                }));
+            }
+        }
+
+        public ICommand DeleteProducerCommand
+        {
+            get
+            {
+                return DeleteProducer ?? (DeleteProducer = new RelayCommand(() =>
+                {
+                    if (SelectedProducer != null)
+                    {
+                        producersService.Delete(SelectedProducer.ProducerId);
+                        SelectedProducer = null;
+                        Producers = producersService.GetAll();
+                        Name = "";
+                        Country = "";
+                    }
+                }));
+            }
+        }
+
+        public ICommand RefreshFieldsCommand
+        {
+            get
+            {
+                return RefreshFields ?? (RefreshFields = new RelayCommand(() =>
+                {
+                    SelectedProducer = null;
+                    Name = "";
+                    Country = "";
+                }));
+            }
+        }
+
     }
 }
